5a66c5a164c0b08a314ca865987ca09c752434ba
Michal Michalski <michalski82@gmail.com>
2020-01-06 21:37:04 +0000

    commit

--- script.R
+++ script.R
@@ -1,1 +1,13 @@
 pagedown::chrome_print("poster.Rmd")
+
+![Figure 1](figures/assyria_empire.png){width=100%}
+
+![Figure 2](figures/lcp_aorist_multi.png){ width=100% }
+
+![Figure 2](figures/lcp.png)
+
+![Figure 2](figures/lcp_dem_map.png){width=100%}
+
+$$
+  A_i = \sum_{i=1}^n O_j f(d_{ij})
+$$
